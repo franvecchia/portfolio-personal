c4fe3f801e5f90ce842e4030672d6719d5fac4c5
Francisco Vecchia <franvecchia00@gmail.com>
2024-01-09 17:52:27 -0300

    proyecto

---
+++ main.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('#btn');
+
+button.addEventListener('click', () => {
+    const nombreApellido= document.querySelector('#nombreApellido');
+    const email=document.querySelector('#email');
+    const mensaje=document.querySelector('#mensaje');
+    if (nombreApellido.value.trim() === '' || email.value.trim() === '' || mensaje.value.trim() === '' || !validarEmail(email.value)) {
+        Swal.fire({
+            title: 'Error',
+            text: 'Faltan rellenar campos o el formato del mail es incorrecto.',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    } else {
+        Swal.fire({
+            title: 'Formulario enviado!',
+            icon: 'success',
+            confirmButtonText: 'Ok'
+        })
+    }
+})
+
+function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
