eb371fc00ec53b55e505a8621835ddc3c5a2d02d
Francisco Vecchia <franvecchia00@gmail.com>
2025-03-23 14:42:47 -0300

    fix

---
+++ js/data.js
@@ -0,0 +1,74 @@
+export const skills = [
+    {
+        id: 1,
+        name: "HTML",
+        img: "../imagenes/iconos/habilidades/html.png"
+    },
+    {
+        id: 2,
+        name: "CSS",
+        img: "../imagenes/iconos/habilidades/css-3.png"
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        img: "../imagenes/iconos/habilidades/js.png"
+    },
+    {
+        id: 4,
+        name: "React.JS",
+        img: "../imagenes/iconos/habilidades/react.png"
+    },
+    {
+        id: 5,
+        name: "Git",
+        img: "../imagenes/iconos/habilidades/git.png"
+    },
+    {
+        id: 6,
+        name: "C++",
+        img: "../imagenes/iconos/habilidades/C++.png"
+    },
+    {
+        id: 7,
+        name: "Java",
+        img: "../imagenes/iconos/habilidades/java.png"
+    },
+    {
+        id: 8,
+        name: "Node.JS",
+        img: "../imagenes/iconos/habilidades/nodejs.png"
+    },
+    {
+        id: 1,
+        name: "SQL",
+        img: "../imagenes/iconos/habilidades/mysql.png"
+    }
+]
+
+export const projects = [
+    {
+        id: 1,
+        img: "../imagenes/calculadora.png",
+        title: "Calculadora",
+        description: "Proyecto en el que simulo una calculadora básica y con las funciones principales. Este proyecto fue el primero que creé con JavaScript, y fue para comenzar a mecanizar las habilidades básicas e intermedias del lenguaje.",
+        code: "https://github.com/franvecchia/proyecto-javascript.git",
+        page: "https://franvecchia.github.io/proyecto-javascript/"
+    },
+    {
+        id: 2,
+        img: "../imagenes/sportify.png",
+        title: "Sportify",
+        description: "Este fue mi primera pagina web simulando un e-commerce deportivo. Lo comencé solamente diseñando la maqueta con HTML y CSS, pero luego le agregué JavaScript simulando un carrito de compras y un formulario.",
+        code: "https://github.com/franvecchia/Proyecto-Html-Css",
+        page: "https://franvecchia.github.io/Proyecto-Html-Css/index.html",
+    },
+    {
+        id: 3,
+        img: "../imagenes/cofax-hardware.png",
+        title: "Cofax Hardware",
+        description: "Proyecto creado 100% con React.JS, en el que simulo una tienda de venta de componentes de hardware de PC, aplicando todas las utilidades y funciones que este framework brinda.",
+        code: "https://github.com/franvecchia/proyecto-react",
+        page: "",
+    }
+]
