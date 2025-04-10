7dc2f4de435710e2c3d7918bb8dc5c995ab33bef
Francisco Vecchia <franvecchia00@gmail.com>
2025-04-09 21:59:57 -0300

    fix bug imagenes

--- js/data.js
+++ js/data.js
@@ -2,54 +2,54 @@ export const skills = [
     {
         id: 1,
         name: "HTML",
-        img: "../imagenes/iconos/habilidades/html.png"
+        img: "imagenes/iconos/habilidades/html.png"
     },
     {
         id: 2,
         name: "CSS",
-        img: "../imagenes/iconos/habilidades/css-3.png"
+        img: "imagenes/iconos/habilidades/css-3.png"
     },
     {
         id: 3,
         name: "JavaScript",
-        img: "../imagenes/iconos/habilidades/js.png"
+        img: "imagenes/iconos/habilidades/js.png"
     },
     {
         id: 4,
         name: "React.JS",
-        img: "../imagenes/iconos/habilidades/react.png"
+        img: "imagenes/iconos/habilidades/react.png"
     },
     {
         id: 5,
         name: "Git",
-        img: "../imagenes/iconos/habilidades/git.png"
+        img: "imagenes/iconos/habilidades/git.png"
     },
     {
         id: 6,
         name: "C++",
-        img: "../imagenes/iconos/habilidades/C.png"
+        img: "imagenes/iconos/habilidades/c.png"
     },
     {
         id: 7,
         name: "Java",
-        img: "../imagenes/iconos/habilidades/java.png"
+        img: "imagenes/iconos/habilidades/java.png"
     },
     {
         id: 8,
         name: "Spring",
-        img: "../imagenes/iconos/habilidades/springboot.png"
+        img: "imagenes/iconos/habilidades/springboot.png"
     },
     {
         id: 9,
         name: "SQL",
-        img: "../imagenes/iconos/habilidades/mysql.png"
+        img: "imagenes/iconos/habilidades/mysql.png"
     }
 ]
 
 export const projects = [
     {
         id: 1,
-        img: "../imagenes/calculadora-alquileres.png",
+        img: "imagenes/calculadora-alquileres.png",
         title: "Calculadora de alquileres",
         description: "Proyecto creado para una inmobiliaria, con un diseño minimalista, que se utiliza como calculadora de alquileres por actualizacion de IPC. Esta pagina utiliza HTML, CSS y Javascript.",
         code: "https://github.com/franvecchia/calculadora-alquileres",
@@ -57,7 +57,7 @@ export const projects = [
     },
     {
         id: 2,
-        img: "../imagenes/sportify.png",
+        img: "imagenes/sportify.png",
         title: "Sportify",
         description: "Este fue mi primera pagina web simulando un e-commerce deportivo. Lo comencé solamente diseñando la maqueta con HTML y CSS, pero luego le agregué JavaScript simulando un carrito de compras y un formulario.",
         code: "https://github.com/franvecchia/Proyecto-Html-Css",
@@ -65,7 +65,7 @@ export const projects = [
     },
     {
         id: 3,
-        img: "../imagenes/cofax-hardware.png",
+        img: "imagenes/cofax-hardware.png",
         title: "Cofax Hardware",
         description: "Proyecto creado 100% con React.JS, en el que simulo una tienda de venta de componentes de hardware de PC, aplicando todas las utilidades y funciones que este framework brinda.",
         code: "https://github.com/franvecchia/proyecto-react",
